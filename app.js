95a46dcc5a334d7bc27935621a0d26560edba205
Julia <julia.golovko23@gmail.com>
2021-10-12 22:25:33 +0300

    change

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 
 let str1 = 'JAVASCRIPT is the Programming Language for the Web. JAVASCRIPT can update and change both HTML and CSS.'
 
-const countVowels = (str) => str.match(/[a,o,i,u,e]/ig).length
+
+const countVowels2 = (str) => {
+  return str.toLowerCase().split('').filter(el => 'a,o,i,u,e'.includes(el)).length
+}
+
+console.log(countVowels2(str1))
 
 
-console.log(countVowels(str1)) 
+
+// const countVowels = (str) => str.match(/[a,o,i,u,e]/ig).length
+
+// console.log(countVowels(str1)) 
+
+
