c533d41c77c4ebd73e82c335e5a2f05cd9f5e9cf
Julia <julia.golovko23@gmail.com>
2021-10-12 19:34:24 +0300

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let str1 = 'JAVASCRIPT is the Programming Language for the Web. JAVASCRIPT can update and change both HTML and CSS.'
+
+const countVowels = (str) => str.match(/[a,o,i,u,e]/ig).length
+
+
+console.log(countVowels(str1)) 
